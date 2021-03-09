e5245ce559a6cee9558b89410088cad88826b512
Victor Santos <victorsantos425@gmail.com>
2021-03-09 13:35:44 -0800

    removed individual python files

--- Reverse_Input.py
+++
@@ -1,9 +0,0 @@
-#This program reverses the user input
-def inputreverse(ui):
-    words = ui.split(' ')
-    reversed_input = ' '.join(reversed(words))
-    return reversed_input
-
-s = input("What would you like to reverse?" "\n") 
-print(inputreverse(s))
-
